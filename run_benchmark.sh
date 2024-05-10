8dc93dd365ecd632cbc5335b937147bf98c6d40d
desubhan <desubhan123@gmail.com>
2024-05-10 08:56:22 +0000

    feat: Add SET, GET, and TTL command evaluation and response
    This commit adds the evaluation and response logic for the SET, GET, and TTL commands in the synchronous TCP server. It introduces the `evalSET`, `evalGET`, and `evalTTL` functions in the `eval.go` file, which handle the respective commands and send the appropriate responses back to the client. These functionalities allow users to set a key-value pair, retrieve the value of a key, and check the time-to-live (TTL) of a key.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,3 +1,5 @@
 clear
-redis-benchmark -n 10000 -t ping_mbulk -c 1 -h localhost -p 6379
-redis-benchmark -n 10000 -t ping_mbulk -c 1 -h localhost -p 7379
+# redis-benchmark -n 10000 -t ping_mbulk -c 1 -h localhost -p 6379
+# redis-benchmark -n 10000 -t ping_mbulk -c 1 -h localhost -p 7379
+redis-benchmark -n 10000 -t ping_mbulk -c 200 -h localhost -p 6379
+redis-benchmark -n 10000 -t ping_mbulk -c 200 -h localhost -p 7379
